a8771f08d9018ade88dcbbe64aab103c3e7c5145
Sergeev Danila <Danilasergeev2003@icloud.com>
2022-11-02 23:41:45 +0300

    add form btn

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,28 @@
 let SwitchBtn =  document.querySelector('.footer__switch-btn');
+let formButton = document.querySelector('.footer__form-button');
+let form = document.querySelector('.footer__form');
+let footer = document.querySelector('.footer');
 
   SwitchBtn.addEventListener('click', () => {
     SwitchBtn.classList.toggle('footer__switch-on');
   });
+  form.addEventListener('mouseover', () => {
+    formButton.classList.add('footer__form-button_on');
+  });
+  form.addEventListener('click', () => {
+    formButton.classList.add('footer__form-button_on');
+  });
+  formButton.addEventListener('mouseover', () => {
+    formButton.classList.add('footer__form-button_on');
+  });
+  form.addEventListener('mouseleave', () => {
+    formButton.classList.remove('footer__form-button_on');
+  });
+/*   footer.addEventListener('click', () => {
+    formButton.classList.remove('footer__form-button_on');
+  }); */
+
+
+ 
+
   
