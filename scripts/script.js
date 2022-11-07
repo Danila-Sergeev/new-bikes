2f1a7da23e34d4d9aa8f05e9e7ab87b4d365b7b8
Danila Sergeev <danilasergeev2003@icloud.com>
2022-11-07 22:36:01 +0300

    add all adoptation

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,24 @@
-let SwitchBtn =  document.querySelector('.footer__switch-btn');
-let formButton = document.querySelector('.footer__form-button');
-let form = document.querySelector('.footer__form');
-let footer = document.querySelector('.footer');
+const SwitchBtn =  document.querySelector('.footer__switch-btn');
+const formButton = document.querySelector('.footer__form-button');
+const form = document.querySelector('.footer__form');
+const footer = document.querySelector('.footer');
+const bikesBtn = document.querySelector(".bikes__button");
+const bikesLink = document.querySelector(".bikes__links");
+const bikesL1 = document.querySelector('#link1');
+const bikesL2 = document.querySelector('#link2');
+const bikesL3 = document.querySelector('#link3');
+const bikesItem = document.querySelector('.bikes__item');
+const bikesIcon = document.querySelector('.bikes__icon');
+const bikesCont = document.querySelector('.bikes__img-cont');
+const sliderMiniFst = document.querySelector('#slider_1')
+const sliderMiniSnd = document.querySelector('#slider_2')
+const sliderMiniThd = document.querySelector('#slider_3')
+const menuBurger = document.querySelector(".header__burger-menu");
+const menuMain = document.querySelector(".header__menu-сontainer");
+const buttonClose = document.querySelector(".header__button-close");
+const buttonArrow = document.querySelector(".header__button-arrow");
+const content = document.querySelector(".content");
+const sliderArrow = document.querySelector(".bikes__arrow")
 
   SwitchBtn.addEventListener('click', () => {
     SwitchBtn.classList.toggle('footer__switch-on');
@@ -23,36 +40,58 @@ let footer = document.querySelector('.footer');
   }); */
 
 
-let bikesBtn = document.querySelector(".bikes__button");
-let bikesLink = document.querySelector(".bikes__links");
-let bikesL1 = document.querySelector('#link1');
-let bikesL2 = document.querySelector('#link2');
-let bikesL3 = document.querySelector('#link3');
-let bikesItem = document.querySelector('.bikes__item');
-let bikesIcon = document.querySelector('.bikes__icon');
-let bikesCont = document.querySelector('.bikes__img-cont');
-let sliderMiniFst = document.querySelector('#slider_1')
-let sliderMiniSnd = document.querySelector('#slider_2')
-let sliderMiniThd = document.querySelector('#slider_3')
-
 bikesBtn.addEventListener('click', () =>{
-  bikesLink.classList.toggle('bikes__links_open')
+  bikesLink.classList.toggle('bikes__links_open');
+  sliderArrow.classList.toggle('bikes__arrow_open');
 })
 bikesL1.addEventListener('click', () => {
   sliderMiniFst.classList.remove('bikes__slider_active');
   sliderMiniSnd.classList.add('bikes__slider_active');
   sliderMiniThd.classList.add('bikes__slider_active');
-  bikesLink.classList.toggle('bikes__links_open')
+  bikesLink.classList.toggle('bikes__links_open');
+  bikesBtn.textContent = bikesL1.textContent;
 })
 bikesL2.addEventListener('click', () => {
   sliderMiniSnd.classList.remove('bikes__slider_active');
   sliderMiniFst.classList.add('bikes__slider_active');
   sliderMiniThd.classList.add('bikes__slider_active');
   bikesLink.classList.toggle('bikes__links_open')
+  bikesBtn.textContent = bikesL2.textContent;
 })
 bikesL3.addEventListener('click', () => {
   sliderMiniThd.classList.remove('bikes__slider_active');
   sliderMiniSnd.classList.add('bikes__slider_active');
   sliderMiniFst.classList.add('bikes__slider_active');
   bikesLink.classList.toggle('bikes__links_open')
+  bikesBtn.textContent = bikesL3.textContent;
 })
+
+if (menuBurger) {
+  menuBurger.addEventListener("click", function (e) {
+    menuMain.classList.add("header__burger-menu_active");
+    buttonClose.classList.add("header__buttons_active");
+    content.classList.add("content_none");
+  });
+}
+
+function closeMenuBurger(menuBurger) {
+  menuBurger.classList.remove("header__burger-menu_active");
+  buttonClose.classList.remove("header__buttons_active");
+  content.classList.remove("content_none");
+}
+
+buttonClose.addEventListener("click", function (e) {
+  closeMenuBurger(menuMain);
+});
+
+if (menuAbout) {
+  aboutFund.addEventListener("click", function(e) {
+    menuAbout.classList.add("header__burger-menu_active");
+    buttonArrow.classList.add("header__buttons_active");
+  });
+}
+
+  buttonArrow.addEventListener("click", function (e) {
+    closeMenuPrograms(menuPrograms);
+  });
+
