ac2701575280c3dbcc4a7c4934e2201c3e6c8496
Danila Sergeev <danilasergeev2003@icloud.com>
2022-11-08 01:24:50 +0300

    add links, start darck theme

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-const SwitchBtn =  document.querySelector('.footer__switch-btn');
+const SwitchBtn =  document.querySelector('#footer__switch-btn');
+const SwitchBtnHeader =  document.querySelector('#header__switch-btn');
+const page = document.querySelector(".page__padding");
+const pagePadding = document.querySelector(".page");
+const body = document.querySelector(".body");
+
 const formButton = document.querySelector('.footer__form-button');
 const form = document.querySelector('.footer__form');
 const footer = document.querySelector('.footer');
@@ -18,10 +23,21 @@ const menuMain = document.querySelector(".header__menu-сontainer");
 const buttonClose = document.querySelector(".header__button-close");
 const buttonArrow = document.querySelector(".header__button-arrow");
 const content = document.querySelector(".content");
-const sliderArrow = document.querySelector(".bikes__arrow")
+const sliderArrow = document.querySelector(".bikes__arrow");
+const humbLinkF = document.querySelector("#header__link-item1");
+const humbLinkS = document.querySelector("#header__link-item2");
+const humbLinkT = document.querySelector("#header__link-item3");
 
   SwitchBtn.addEventListener('click', () => {
     SwitchBtn.classList.toggle('footer__switch-on');
+    page.classList.toggle('black-theme');
+    pagePadding.classList.toggle('black-theme');
+    body.classList.toggle('black-theme');
+    footer.classList.toggle('footer_darck');
+
+  });
+  SwitchBtnHeader.addEventListener('click', () => {
+    SwitchBtnHeader.classList.toggle('footer__switch-on');
   });
   form.addEventListener('mouseover', () => {
     formButton.classList.add('footer__form-button_on');
@@ -84,14 +100,26 @@ buttonClose.addEventListener("click", function (e) {
   closeMenuBurger(menuMain);
 });
 
-if (menuAbout) {
-  aboutFund.addEventListener("click", function(e) {
-    menuAbout.classList.add("header__burger-menu_active");
-    buttonArrow.classList.add("header__buttons_active");
-  });
+humbLinkF.addEventListener('click', function (e) {
+  closeMenuBurger(menuMain);
+});
+humbLinkS.addEventListener('click', function (e) {
+  closeMenuBurger(menuMain);
+});
+humbLinkT.addEventListener('click', function (e) {
+  closeMenuBurger(menuMain);
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
-
-  buttonArrow.addEventListener("click", function (e) {
-    closeMenuPrograms(menuPrograms);
-  });
-
